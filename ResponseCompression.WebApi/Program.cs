2af4e50f17ee84e5ef770de0cba80bbdbfcd6c5b
Selcuk <slckarslan93@gmail.com>
2025-04-06 15:30:15 +0300

    Add response compression and Swagger integration
    This commit introduces response compression support in the ASP.NET Core application, utilizing the Brotli compression provider with the 'Fastest' compression level. It also integrates Swagger for API documentation and updates the application pipeline to include Swagger UI and response compression. Additionally, a new endpoint `getList` is created to generate a list of integers from 0 to 999.

--- ResponseCompression.WebApi/Program.cs
+++ ResponseCompression.WebApi/Program.cs
@@ -1,7 +1,30 @@
+using Microsoft.AspNetCore.ResponseCompression;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+	options.Providers.Add<BrotliCompressionProvider>(); //Default ve hizli guclu
+	//options.Providers.Add<GzipCompressionProvider>(); //eski browserlar için
+});
+
+builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+{
+    //options.Level = System.IO.Compression.CompressionLevel.Optimal; 
+    options.Level = System.IO.Compression.CompressionLevel.Fastest; 
+});
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
 
-app.MapGet("/", () => 
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseResponseCompression();
+
+app.MapGet("getList", () => 
 {
     List<int> numbers = new();
 	for (int i = 0; i < 1000; i++)
